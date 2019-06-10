056b7b95c722780d61851eba215aa9aea960bf65
elitegoliath <elitegoliath@hotmail.com>
2019-06-09 22:12:30 -0500

    Fiddling around a bit with the structure of the actions.

--- d2gb-client/src/state/actions/images.actions.ts
+++ d2gb-client/src/state/actions/images.actions.ts
@@ -15,13 +15,12 @@ export const recieveIconURLs = (_iconURLs: List<iImageURL>): iReduxAction => ({
 });
 
 export const fetchIconURLs = () => {
-    // TODO: stringly type and get an interface for the destructed 3rd argument object to make it easier to develop with.
     return (_dispatch: any, _getState: any, { getFirebase, getFirestore }: any): void => {
         // TODO: Add property ot state for whether icons have been loaded or are loading to prevent multiple calls.
 
         // TODO: Make an abstract class to handle async call to the firestore.
         // TODO: Make a constant for each collection name.
-        
+
         (async () => {
             try {
                 const request = await getFirestore().collection('icon_urls').get();
@@ -30,16 +29,14 @@ export const fetchIconURLs = () => {
                     iconUrlCollect.push(_doc.data());
                 });
                 console.log('Got icon URLs: ', iconUrlCollect);
+
                 // SUCCESS DISPATCH
+                const fakeList = List();
+                _dispatch(recieveIconURLs(fakeList));
             } catch (_e) {
-                console.log('Issues getting something from firestore.')
                 // ERROR DISPATCH
+                console.log('Issues getting something from firestore.')
             }
         })();
-        
-
-        
-        const fakeList = List();
-        _dispatch(recieveIconURLs(fakeList));
     };
 }
