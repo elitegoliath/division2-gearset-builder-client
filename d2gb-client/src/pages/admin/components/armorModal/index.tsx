666698f93ca524f3949754091269330b866c5219
elitegoliath <elitegoliath13@gmail.com>
2019-07-07 19:20:58 -0500

    fixed a mistake with field placement on the admin modal form

--- d2gb-client/src/pages/admin/components/armorModal/index.tsx
+++ d2gb-client/src/pages/admin/components/armorModal/index.tsx
@@ -66,30 +66,31 @@ export const ArmorModal = (_props: iArmorModal) => {
             <Modal.Content>
                 <Form inverted onSubmit={handleSubmit}>
                     {isAddingNew ?
-                    <div>
-                        <Header as='h1' color='teal' textAlign='center'>Editing Existing Armor</Header>
-                        <Button size='small' color='orange' style={modeButtonStyle} onClick={changeMode}>Add New Armor</Button>
-                        <FormGroup widths='equal'>
-                            <Form.Input placeholder='Armor Model...' name='model' label='Armor Model' />
-                            <Form.Select placeholder='Armor Type...' name='type' options={typeOptions} label='Armor Type' />
-                        </FormGroup>
-                    </div> :
-                    <div>
-                        <Header as='h1' color='teal' textAlign='center'>Adding New Armor</Header>
-                        <Button size='small' color='orange' style={modeButtonStyle} onClick={changeMode}>Edit Existing Armor</Button>
-                        <FormGroup widths='equal'>
-                            {armorNameList ?
-                                <Form.Select
-                                    label='Select Existing Armor'
-                                    placeholder='Select Existing Armor...'
-                                    options={armorNameList.toJS()}
-                                    onChange={onArmorSelected}
-                                ></Form.Select> :
-                                ''
-                            }
-                            <Form.Select placeholder='Armor Type...' name='type' options={typeOptions} label='Armor Type' value={armorType} />
-                        </FormGroup>
-                    </div>}
+                        <div>
+                            <Header as='h1' color='teal' textAlign='center'>Adding New Armor</Header>
+                            <Button size='small' color='orange' style={modeButtonStyle} onClick={changeMode}>Edit Existing Armor</Button>
+                            <FormGroup widths='equal'>
+                                <Form.Input placeholder='Armor Model...' name='model' label='Armor Model' />
+                                <Form.Select placeholder='Armor Type...' name='type' options={typeOptions} label='Armor Type' />
+                            </FormGroup>
+                        </div> :
+                        <div>
+                            <Header as='h1' color='teal' textAlign='center'>Editing Existing Armor</Header>
+                            <Button size='small' color='orange' style={modeButtonStyle} onClick={changeMode}>Add New Armor</Button>
+                            <FormGroup widths='equal'>
+                                {armorNameList ?
+                                    <Form.Select
+                                        label='Select Existing Armor'
+                                        placeholder='Select Existing Armor...'
+                                        options={armorNameList.toJS()}
+                                        onChange={onArmorSelected}
+                                    ></Form.Select> :
+                                    ''
+                                }
+                                <Form.Select placeholder='Armor Type...' name='type' options={typeOptions} label='Armor Type' value={armorType} />
+                            </FormGroup>
+                        </div>
+                    }
                     <Form.Button color='red' onClick={closeArmorModal}>Cancel</Form.Button>
                     <Form.Button color='green' content='Submit' />
                 </Form>
